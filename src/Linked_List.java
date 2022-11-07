64b7bca2832b53f71e3cff1c6fe80fbe3523022c
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2022-11-07 23:20:11 +0530

    day 1 Linked List

---
+++ src/Linked_List.java
@@ -0,0 +1,18 @@
+public class Linked_List {
+
+    class Node{
+        int data;
+          Node tail;
+        Node(int _data){
+             this.data=_data;
+             this.tail=null;
+        }
+    }
+    Node head=null;
+
+
+
+
+
+
+}
