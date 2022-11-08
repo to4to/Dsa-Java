d5b55b788bd99fb88e99e4322a59ba1df56bb833
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2022-11-08 23:18:04 +0530

    Circular LL Creation +Traversal

---
+++ src/Circular_LL.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Circular_LL {
+    Scanner sc=new Scanner(System.in);
+    public class Node{
+        int data;
+        Node next;
+        Node(int _data){
+            this.data=_data;
+            this.next=null;
+        }
+    }
+
+    Node head;
+    Node tail;
+
+
+    public void creation(){
+        int data,n;
+        do{
+
+            System.out.println("Enter Data");
+            data=sc.nextInt();
+            Node new_node=new Node(data);
+            if(head==null){
+                head=new_node;
+                tail=new_node;
+            }else{
+                //inserting in begning
+                new_node.next=head;
+                head=new_node;
+                tail.next=head;
+            }
+
+            System.out.println("Enter 1 to Continue");
+            n= sc.nextInt();
+
+
+        }while (n==1);
+    }
+    public  void traverse(){
+        Node temp=head;
+
+        if(head==null){
+            System.out.println("Circular LL Empty");
+        }else{
+            do{
+                System.out.println(temp.data);
+                temp=temp.next;
+
+
+            }while(temp!=head);
+
+
+        }
+
+
+    }
+
+
+    public static  void main(String R[]){
+
+        Circular_LL circular_ll=new Circular_LL();
+circular_ll.creation();
+circular_ll.traverse();
+
+
+
+    }
+
+
+
+}
