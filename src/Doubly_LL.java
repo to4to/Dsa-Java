2fbb069a393831faeeceeafa51db37c1d5ac0f83
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2022-11-11 14:21:15 +0530

    Doubly LL adding insert+traverse

--- src/Doubly_LL.java
+++ src/Doubly_LL.java
@@ -42,8 +42,72 @@ Scanner sc= new Scanner(System.in);
             n=sc.nextInt();
 
         }while(n==1);
-
     }
+
+    public  void traverse(){
+
+
+        Node temp=head;
+        if(head==null){
+            System.out.println("No Element in List");
+        }else {
+            while (temp!=null){
+                System.out.println(temp.data);
+                temp=temp.next;
+            }
+        }
+    }
+public void insert(){
+        int data,n,m,pos;
+        do {
+            System.out.println("Enter Data");
+            data=sc.nextInt();
+
+            Node new_node=new Node(data);
+            if (head==null){
+                head=new_node;
+                tail=new_node;
+            }else {
+                System.out.println("1 for Begning ,2 For End & 3 for Specific position");
+                m=sc.nextInt();
+                switch (m){
+
+                    case 1:
+                        new_node.next=head;
+                        head.prev=new_node;
+                        head=new_node;
+                        break;
+
+                    case 2:
+                        tail.next=new_node;
+                        new_node.prev=tail;
+                        tail=new_node;
+                        break;
+
+                    case 3:
+                        System.out.println("Ente Position ");
+                        pos=sc.nextInt();
+
+                        Node temp1=head;
+                        Node ptr=temp1.next;
+
+                        for (int i=1;i<(pos-1);i++){
+
+
+                            temp1=ptr;
+                            ptr=ptr.next;
+                        }
+                        new_node.prev=temp1;
+                        new_node.next=ptr;
+                        temp1.next=new_node;
+                        ptr.prev=new_node;
+                        break;
+                }
+            }
+            System.out.println("Press 1 to Continue");
+            n= sc.nextInt();
+        }while (n==1);
+}
 
 
 }
