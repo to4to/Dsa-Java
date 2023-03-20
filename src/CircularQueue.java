dd108daef9543207fba040078f47369fb9934f4f
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-03-20 21:30:14 +0530

    Circular Queue Complete

--- src/CircularQueue.java
+++ src/CircularQueue.java
@@ -1,10 +1,63 @@
+import java.util.Scanner;
+
 public class CircularQueue {
-    int f=-1,r=-1;
-    int n=10;
+   static int f=-1,r=-1;
+ static    int n=10;
+static Scanner sc=new Scanner(System.in);
+    static int[] queue=new int[n];
+
+   static void enqueue(int _data){
+        if(f==(r+1)%n){
+            System.out.println("OverFlow");
+        }else if (f==-1 && r==-1){
+            f++;
+            r++;
+            queue[r]=_data;
+        }else{
+            r=(r+1)%n;
+            queue[r]=_data;
+        }
+    }
 
 
 
 
+   static void dequeue(){
+
+        if(f==-1&&r==-1 || f==r){
+            System.out.println("UnderFlow");
+        }else{
+            f=(f+1)%n;
+        }
+    }
+
+
+ static void dispaly(){
+
+for(int i=f;i!=r;i=(i+1)%n){
+    System.out.println(queue[i]);
+}
+}
+
+    public static void main(String[] args) {
+        System.out.println("1:Enqueue\n2:Dequeue\n3:Display");
+        int choice=sc.nextInt();
+
+        switch (choice){
+            case 1->{
+                System.out.println("Enter Data");
+            int data=sc.nextInt();
+            enqueue(data);
+            }
+            case 2->dequeue();
+            case 3->dispaly();
+            default -> System.out.println("Wrong Input");
+
+
+        }
+
+
+    }
 
 
 }
