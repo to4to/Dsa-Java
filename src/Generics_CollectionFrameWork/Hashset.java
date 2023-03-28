741f710ea41a9e05ce137c2582a1fad8513d02c6
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-03-28 23:30:52 +0530

    Adding HashSet

---
+++ src/Generics_CollectionFrameWork/Hashset.java
@@ -0,0 +1,22 @@
+package Generics_CollectionFrameWork;
+
+
+import java.util.HashSet;
+
+public class Hashset {
+    public static void main(String[] args) {
+
+        HashSet<Integer> set= new HashSet<Integer>();
+
+        set.add(11);
+
+
+
+
+
+    }
+
+
+
+
+}
