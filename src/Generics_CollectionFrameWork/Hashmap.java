455b745eb68ea1368011c45dd9dea44fe8049cbb
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-03-31 01:01:02 +0530

    Adding Some Map functions

--- src/Generics_CollectionFrameWork/Hashmap.java
+++ src/Generics_CollectionFrameWork/Hashmap.java
@@ -1,4 +1,30 @@
 package Generics_CollectionFrameWork;
-
+import java.util.HashMap;
 public class Hashmap {
+
+
+    public static void main(String[] args) {
+
+        HashMap<Integer,Integer>map= new HashMap<Integer, Integer>();
+
+map.put(1, 100);
+map.put(2, 200);
+map.put(3, 300);
+map.get(1);
+
+
+map.containsKey(1);
+map.containsValue(100);
+map.isEmpty();
+map.size();
+map.keySet();
+map.values();
+
+
+
+
+
+    }
+
+
 }
