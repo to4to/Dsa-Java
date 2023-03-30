af9e09f70149f7ea71214a2016421d963d982202
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-03-31 01:03:28 +0530

    Adding some more hash functions

--- src/Generics_CollectionFrameWork/Hashmap.java
+++ src/Generics_CollectionFrameWork/Hashmap.java
@@ -19,6 +19,29 @@ map.isEmpty();
 map.size();
 map.keySet();
 map.values();
+map.entrySet();
+map.hashCode();
+map.equals(map);
+map.clone();
+map.clear();
+map.remove(1);
+map.remove(1, 100);
+map.replace(1, 100, 1000);
+map.replace(1, 1000);
+map.putIfAbsent(1, 1000);
+map.putIfAbsent(1, 100);
+map.compute(1, (k,v)->v+100);
+map.computeIfAbsent(1, k->100);
+map.computeIfPresent(1, (k,v)->v+100);
+map.merge(1, 100, (v1,v2)->v1+v2);
+map.merge(1, 100, (v1,v2)->v1+v2);
+map.merge(1, 100, (v1,v2)->v1+v2);
+map.merge(1, 100, (v1,v2)->v1+v2);
+
+map.forEach((k,v)->System.out.println(k+" "+v));
+
+
+
 
 
 
