761f1ea1b7c1ffe94899f971640dd89b444c2e73
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-05-27 23:22:20 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello it's, I to4to Missed U :)");
+        System.out.println("Hello it's, to4to Missed U :)");
     }
 }
